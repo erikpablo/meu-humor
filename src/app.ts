11358c7bd7d996e5ae9d72fda0e444de895da788
Erik Pablo <nunespablo470@gmail.com>
2025-09-23 08:47:28 -0300

    feat: :sparkles: create set error handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
 import fastify from 'fastify'
+import { env } from './env'
+import { ZodError } from 'zod'
 
 export const app = fastify()
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'validation error',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: enviar para um serviço de monitoramento tool datadog/newRelic
+  }
+
+  reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
+
+// Criamos o app.setErrorHandler() para tratar erros de forma global
