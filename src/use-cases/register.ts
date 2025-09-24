fd796e2e27dfcbad249671b9fc8eb9255b38c317
Erik Pablo <nunespablo470@gmail.com>
2025-09-24 08:55:32 -0300

    feat: :sparkles: created route register

---
+++ src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import type { User } from '@prisma/client'
+import type { UserRepository } from 'src/repositories/users-repository'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UserRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password,
+    })
+
+    return {
+      user,
+    }
+  }
+}
