2a27ed2c0e3905833dfa7c43fd8e7634be54c318
Erik Pablo <nunespablo470@gmail.com>
2025-10-20 07:44:14 -0300

    fix: :bug: import with error

--- src/http/controller/moody/route.ts
+++ src/http/controller/moody/route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/middleware/verify-jwt'
-import { moodEntry } from './mood-Entry'
+import { moodEntry } from './mood-entry'
 
 export async function moodRoutes(app: FastifyInstance) {
   app.post('/mood', { onRequest: [verifyJWT] }, moodEntry)
